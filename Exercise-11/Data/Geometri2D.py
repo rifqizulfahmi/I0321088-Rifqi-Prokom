0cf35265e38c0dc7718f5e988d3c6efc16522a4c
rifqizulfahmi <rifqizulfahmi110303@gmail.com>
2022-06-07 13:08:17 +0700

    Mengerjakan file Exercise-11

---
+++ Exercise-11/Data/Geometri2D.py
@@ -0,0 +1,20 @@
+# Silakan perhatikan perintah pada file 11_Modul&Paket.ipynb sebelum membuka file ini
+def LuasPersegiPanjang(p,l):
+    return p*l
+def KelilingPersegiPanjang(p,l):
+    return 2*(p+l)
+
+def LuasBujurSangkar(s):
+    return s*s
+def KelilingBujurSangkar(s):
+    return 4*s
+
+def LuasLingkaran(r):
+    return math.pi*r*r
+def KelilingLingkaran(r):
+    return 2*math.pi*r
+
+def LuasSegitiga(a,t):
+    return (a*t)/2
+def KelilingSegitiga(a,b,c):
+    return a+b+c
---
+++ Exercise-11/Data/__init__.py
@@ -0,0 +1,2 @@
+# Silakan perhatikan perintah pada file 11_Modul&Paket.ipynb sebelum membuka file ini
+from . import Geometri2D
